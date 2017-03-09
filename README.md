ba99df04fb2818e76725afd51ce287c1a4f7726e
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-03-09 20:28:22 +0300

    The task was completed

--- README.md
+++ README.md
@@ -1,18 +1,28 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+Outputs the contents of the json-file to the console in a convenient format (pretty print).
 
 # Quickstart
 
-[TODO]
-
-Example of script launch on Linux, Python 3.5:
+The example of script launch on Linux, Python 3.5 and you need default.json, which can be downloaded from https://data.mos.ru/opendata/7710881420-magaziny-alkogolnye-napitki. You can specify a different file name.
 
 ```#!bash
 
-$ python pprint_json.py <path to file>
-# TODO add output example
-
+$ python3 pprint_json.py test.json
+Рабочий файл: test.json
+{
+    "address": {
+        "city": "Ленинград",
+        "postalCode": 101101,
+        "streetAddress": "Московское ш., 101, кв.101"
+    },
+    "firstName": "Иван",
+    "lastName": "Иванов",
+    "phoneNumbers": [
+        "812 123-1234",
+        "916 123-4567"
+    ]
+}
 ```
 
 # Project Goals
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,33 @@
 import json
+import sys
+import os
+
+DEFAULT_FILE_NAME = 'default.json'
+DEFAULT_INDENT = 4
 
 
-def load_data(filepath):
-    pass
-
+def load_data(file_path):
+    with open(file_path, 'rt') as json_file:
+        json_string = json_file.read()
+        parsed_string = json.loads(json_string)
+        return parsed_string
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, indent=DEFAULT_INDENT, sort_keys=True, ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        print('Нет параметров для запуска! Файл по умолчанию: {}'.format(DEFAULT_FILE_NAME))
+        work_file = DEFAULT_FILE_NAME
+    else:
+        work_file = sys.argv[1]
+        if os.path.isfile(work_file):
+            print('Рабочий файл: {}'.format(work_file))
+
+    if not os.path.exists(work_file):
+        print('Файл {} не существует'.format(work_file))
+        sys.exit(1)
+
+    data_from_json = load_data(work_file)
+    print(pretty_print_json(data_from_json))
